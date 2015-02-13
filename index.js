06a60ca7e97ef717f88b0cbdb1cf5704af8854d0
Jacob Lauzier <jacob.lauzier@machinemetrics.com>
2015-02-13 10:41:51 -0500

    switched over to using q instead of promise

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 // taken from https://github.com/taskcluster/taskcluster-queue/blob/0dd2b45b860836a2ef6c651c5499c9abfb5670ad/utils/aws-sdk-promise.js
 var aws = require('aws-sdk');
-var Promise = require('promise');
+var Q = require('q');
 
 // XXX: This is a terrible hack but it should not break anything in horrible
 // ways.
-aws.Request.prototype.promise = function() {
-  var that = this;
-  return new Promise(function(accept, reject) {
-    that.on('complete', function(response) {
-      if (response.error) {
-        reject(response.error);
-      } else {
-        accept(response);
-      }
-    });
-    that.send();
+aws.Request.prototype.q = function() {
+  var deferred = Q.defer();
+
+  this.on('complete', function(response) {
+    if(response.error) deferred.reject(new Error(response.error));
+    else deferred.resolve(response.data);
   });
+  this.send();
+
+  return deferred.promise;
 };
 
 module.exports = aws;
